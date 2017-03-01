0db7a4527c0e7e384275d9def3afc7c34cc84597
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-03-01 01:45:44 +0000

    got rid of primary key seeds

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -9,13 +9,13 @@ exports.seed = function(knex, Promise) {
       const hash = bcrypt.hashSync('password', salt);
       return Promise.all([
         // Inserts seed entries
-        knex('users').insert({id: 1, first_name: 'Felipe', last_name: 'Suarez', reward_points: '9000', password_digest: hash, drivers_license: '123456',
+        knex('users').insert({first_name: 'Felipe', last_name: 'Suarez', reward_points: '9000', password_digest: hash, drivers_license: '123456',
           verified_at: date, email: 'felipe@email.com'}),
-        knex('users').insert({id: 2, first_name: 'Arman', last_name: 'Kazemi', reward_points: '10000', password_digest: hash, drivers_license: '123321',
+        knex('users').insert({first_name: 'Arman', last_name: 'Kazemi', reward_points: '10000', password_digest: hash, drivers_license: '123321',
           verified_at: date, email: 'arman@email.com'}),
-        knex('users').insert({id: 3, first_name: 'Jinuk', last_name: 'Kim', reward_points: '9000', password_digest: hash, drivers_license: '654321',
+        knex('users').insert({first_name: 'Jinuk', last_name: 'Kim', reward_points: '9000', password_digest: hash, drivers_license: '654321',
           verified_at: date, email: 'jinuk@email.com'}),
-        knex('users').insert({id: 4, first_name: 'Josh', last_name: 'Tran', reward_points: '9000', password_digest: hash, drivers_license: '321123',
+        knex('users').insert({first_name: 'Josh', last_name: 'Tran', reward_points: '9000', password_digest: hash, drivers_license: '321123',
           verified_at: date, email: 'josh@email.com'}),
       ]);
     });
--- seeds/02_shelter.js
+++ seeds/02_shelter.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('shelter').insert({id: 1, name: 'The Salvation Army Harbour Light', location: '119 E Cordova St', notes: 'Enter from backdoor.', email: 'salvation@email.com', phone_number: '(604) 646-6800' }),
-        knex('shelter').insert({id: 2, name: 'Catholic Charity Mens Hostel', location: '828 Cambie St', notes: 'Food always welcome!', email: 'catholic@email.com', phone_number: '(604) 443-3292' }),
-        knex('shelter').insert({id: 3, name: 'First United Church Community Ministry Society', location: '320 East Hastings Street', notes: 'We really like apples.', email: 'first@email.com', phone_number: '(604) 681-8365' }),
-        knex('shelter').insert({id: 4, name: 'RainCity Housing', location: '185 Alexander St', notes: 'Good housing support, organics always welcome.', email: 'raincity@email.com', phone_number: '(604) 662-7023' }),
-        knex('shelter').insert({id: 5, name: 'Union Gospel Mission', location: '601 E Hastings St', notes: 'Help always welcome!', email: 'union@email.com', phone_number: '(604) 253-3323' })
+        knex('shelter').insert({name: 'The Salvation Army Harbour Light', location: '119 E Cordova St', notes: 'Enter from backdoor.', email: 'salvation@email.com', phone_number: '(604) 646-6800' }),
+        knex('shelter').insert({name: 'Catholic Charity Mens Hostel', location: '828 Cambie St', notes: 'Food always welcome!', email: 'catholic@email.com', phone_number: '(604) 443-3292' }),
+        knex('shelter').insert({name: 'First United Church Community Ministry Society', location: '320 East Hastings Street', notes: 'We really like apples.', email: 'first@email.com', phone_number: '(604) 681-8365' }),
+        knex('shelter').insert({name: 'RainCity Housing', location: '185 Alexander St', notes: 'Good housing support, organics always welcome.', email: 'raincity@email.com', phone_number: '(604) 662-7023' }),
+        knex('shelter').insert({name: 'Union Gospel Mission', location: '601 E Hastings St', notes: 'Help always welcome!', email: 'union@email.com', phone_number: '(604) 253-3323' })
       ]);
     });
 };
