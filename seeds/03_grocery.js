450bc39acc52f0ea2192d2bb9b566da6aa84866c
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-03-03 05:43:54 +0000

    grocery seed

--- seeds/03_grocery.js
+++ seeds/03_grocery.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('grocery').insert({name: 'IGA', location: '909 Burrard St', notes: 'Talk to Joe at the counter.', email: 'iga@email.com', phone_number: '(604) 605-0612'}),
+        knex('grocery').insert({name: 'IGA', location: '909 Burrard St', notes: 'Talk to Joe at the counter.', email: 'iga@email.com', phone_number: '(604) 605-0612', picture:'https://igav3-metcdn-com.global.ssl.fastly.net/content/uploads/2016/10/19231144/iga-australia-logo.png'}),
         knex('grocery').insert({name: 'Nesters Market', location: '990 Seymour St', notes: 'Come in from the side door.', email: 'nesters@email.com', phone_number: '(604) 682-3071'}),
         knex('grocery').insert({name: 'Foodness', location: '927 Seymour St', notes: 'Talk to Jimmy for more information.', email: 'foodness@email.com', phone_number: '(604) 568-8646'}),
         knex('grocery').insert({name: 'Famous Foods', location: '1595 Kingsway St', notes: 'Come in through the bulk foods section.', email: 'famous@email.com', phone_number: '(604) 872-3019'}),
