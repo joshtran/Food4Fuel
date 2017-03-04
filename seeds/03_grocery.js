803891ae3128f32a7a182d125e8467f36e21a893
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-03-04 05:33:40 +0000

    seeded images for shelters and groceries

--- seeds/03_grocery.js
+++ seeds/03_grocery.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('grocery').insert({name: 'IGA', location: '909 Burrard St', notes: 'Talk to Joe at the counter.', email: 'iga@email.com', phone_number: '(604) 605-0612', picture:'https://igav3-metcdn-com.global.ssl.fastly.net/content/uploads/2016/10/19231144/iga-australia-logo.png'}),
-        knex('grocery').insert({name: 'Nesters Market', location: '990 Seymour St', notes: 'Come in from the side door.', email: 'nesters@email.com', phone_number: '(604) 682-3071'}),
-        knex('grocery').insert({name: 'Foodness', location: '927 Seymour St', notes: 'Talk to Jimmy for more information.', email: 'foodness@email.com', phone_number: '(604) 568-8646'}),
-        knex('grocery').insert({name: 'Famous Foods', location: '1595 Kingsway St', notes: 'Come in through the bulk foods section.', email: 'famous@email.com', phone_number: '(604) 872-3019'}),
-        knex('grocery').insert({name: 'T & T Supermarket', location: '179 Keefer Pl', notes: 'Come in through the restaurant section.', email: 'tandt@email.com', phone_number: '(604) 899-8836'})
+        knex('grocery').insert({name: 'IGA', location: '909 Burrard St', notes: 'Talk to Joe at the counter.', email: 'iga@email.com', phone_number: '(604) 605-0612', picture:'iga.png'}),
+        knex('grocery').insert({name: 'Nesters Market', location: '990 Seymour St', notes: 'Come in from the side door.', email: 'nesters@email.com', phone_number: '(604) 682-3071', picture:'nestersmarket.png'}),
+        knex('grocery').insert({name: 'Foodness', location: '927 Seymour St', notes: 'Come in through the bulk foods section.', email: 'foodness@email.com', phone_number: '(604) 568-8646', picture:'foodness.jpg'}),
+        knex('grocery').insert({name: 'Meinhardt Fine Foods', location: '609 Granville St', notes: 'Talk to Jimmy for more information.', email: 'meinhardt@email.com', phone_number: '(604) 681-8175', picture:'meinhardt.jpg'}),
+        knex('grocery').insert({name: 'H-Mart', location: '590 Robson St #200', notes: 'Come in through the restaurant section.', email: 'hmart@email.com', phone_number: '(604) 609-4567', picture:'hmart.png'})
       ]);
     });
 };
