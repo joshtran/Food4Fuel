8b6e92cb958a8406c2099a572241755472217155
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-03-05 03:06:56 +0000

    added more boxes that don't belong to a package yet

--- seeds/05_box.js
+++ seeds/05_box.js
@@ -14,11 +14,51 @@ exports.seed = function(knex, Promise) {
         knex('box').insert({type: 'Produce', package_id: 3, grocery_id: 1, created_at: date}),
         knex('box').insert({type: 'Baked Goods', package_id: 4, grocery_id: 2, created_at: date}),
         knex('box').insert({type: 'Baked Goods', package_id: 4, grocery_id: 3, created_at: date}),
-        knex('box').insert({type: 'Dairy', grocery_id: 4, created_at: date}),
-        knex('box').insert({type: 'Produce', grocery_id: 5, created_at: date}),
+
         knex('box').insert({type: 'Dairy', grocery_id: 1, created_at: date}),
+        knex('box').insert({type: 'Produce', grocery_id: 1, created_at: date}),
+        knex('box').insert({type: 'Produce', grocery_id: 1, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 1, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 1, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 1, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 1, created_at: date}),
+
+
         knex('box').insert({type: 'Baked Goods', grocery_id: 2, created_at: date}),
-        knex('box').insert({type: 'Produce', grocery_id: 3, created_at: date})
+        knex('box').insert({type: 'Dairy', grocery_id: 2, created_at: date}),
+        knex('box').insert({type: 'Produce', grocery_id: 2, created_at: date}),
+        knex('box').insert({type: 'Produce', grocery_id: 2, created_at: date}),
+        knex('box').insert({type: 'Produce', grocery_id: 2, created_at: date}),
+
+
+        knex('box').insert({type: 'Produce', grocery_id: 3, created_at: date}),
+        knex('box').insert({type: 'Dairy', grocery_id: 3, created_at: date}),
+        knex('box').insert({type: 'Dairy', grocery_id: 3, created_at: date}),
+        knex('box').insert({type: 'Dairy', grocery_id: 3, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 3, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 3, created_at: date}),
+
+
+        knex('box').insert({type: 'Produce', grocery_id: 4, created_at: date}),
+        knex('box').insert({type: 'Produce', grocery_id: 4, created_at: date}),
+        knex('box').insert({type: 'Produce', grocery_id: 4, created_at: date}),
+        knex('box').insert({type: 'Dairy', grocery_id: 4, created_at: date}),
+        knex('box').insert({type: 'Dairy', grocery_id: 4, created_at: date}),
+        knex('box').insert({type: 'Dairy', grocery_id: 4, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 4, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 4, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 4, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 4, created_at: date}),
+
+
+        knex('box').insert({type: 'Produce', grocery_id: 5, created_at: date}),
+        knex('box').insert({type: 'Produce', grocery_id: 5, created_at: date}),
+        knex('box').insert({type: 'Dairy', grocery_id: 5, created_at: date}),
+        knex('box').insert({type: 'Dairy', grocery_id: 5, created_at: date}),
+        knex('box').insert({type: 'Dairy', grocery_id: 5, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 5, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 5, created_at: date}),
+        knex('box').insert({type: 'Baked Goods', grocery_id: 5, created_at: date})
       ]);
     });
 };
