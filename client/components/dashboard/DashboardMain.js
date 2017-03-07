8edc20c6ead9df831aebdd6c13bfd1d87be8625f
David Kim <davidjinuk90@gmail.com>
2017-03-07 10:37:10 +0000

    Created panel for redeeming points for gas. Added photo to pictures folder

--- client/components/dashboard/DashboardMain.js
+++ client/components/dashboard/DashboardMain.js
@@ -5,13 +5,51 @@ class DashboardMain extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>DELIVERY CONFIRMED!</h1>
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          Redeem Points
+        </div>
+        <div className="panel-body">
+          1000 points = $10 fuel card
+          50 points per box
+          {this.props.reward_points}
+          <div className="col-md-5">
+            <div className="box-panel panel panel-default">
+              <img className="panel-img" src="/pictures/dashboard/gas.png" alt="Gas Image"/>
+              <div className = "panel-body">
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    <span className="badge">SOME NUMBER</span>
+                    Redeemable Points
+                  </li>
+                  <li className="list-group-item">
+                    <span className="badge">SOME NUMBER</span>
+                    Points Redeemed
+                  </li>
+                </ul>
+              </div>
+              <div className="panel-footer">
+                <div className="text-center">
+                  <div className="btn-group btn-group-lg" role="group" aria-label="...">
+                    <button type="button" className="btn btn-default">-</button>
+                    <button type="button" className="btn btn-default">Redeem</button>
+                    <button type="button" className="btn btn-default">+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="panel-footer">
+
+        </div>
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  reward_points: state.auth.user.reward_points
+});
 
-export default connect(null)(DashboardMain);
-
+export default connect(mapStateToProps)(DashboardMain);
