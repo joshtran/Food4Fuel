2f89cc57d0f97f2750fedb3a2c31379a8d4bb582
Joshua Tran <joshuatrain.com>
2017-03-12 00:00:16 +0000

    Reorganized folders and project structure.

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
     client: 'postgresql',
     connection: {
-      database: 'final',
-      user:     'food',
-      password: 'food'
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS
     },
     pool: {
       min: 2,
